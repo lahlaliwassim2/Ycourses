7dd97e0cd2f0fa5409b727ff2f87d39dd24b2ad9
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-16 11:55:17 +0100

    Configuration dee routes  pour UserCtrl

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 // Routes
 
 app.use('/api/auth', require('./routes/authRoute'))
-
+app.use('/api/users', require('./routes/usersRoute'))
 //Running To Server
 
 const PORT = process.env.PORT
--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const { getAllUsers, getUser } = require('../controllers/usersControllers')
 const { verifyToken, verifyTokenAndAdmin } = require('../middlewares/verifyToken')
+const validateObjectId = require('../middlewares/validateObjectId')
 // api/users/profile
 router.get('/profil',verifyTokenAndAdmin,getAllUsers)
-router.get('/profil/:id',getUser)
+// api/users/profile/:id
+router.get('/profil/:id',validateObjectId,getUser)
 
 module.exports = router
