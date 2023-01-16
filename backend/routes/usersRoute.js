1cd11ba5827f6891cf952b59c1b6256fad9b4731
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-16 14:26:13 +0100

    Configuration dee routes

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,10 +1,13 @@
 const router = require('express').Router()
-const { getAllUsers, getUser } = require('../controllers/usersControllers')
-const { verifyToken, verifyTokenAndAdmin } = require('../middlewares/verifyToken')
+const { getAllUsers, getUser, updateUserProfilCtrl, getUsersCountCtrl } = require('../controllers/usersControllers')
+const { verifyTokenAndAdmin ,verifyTokenAndOnlyUser} = require('../middlewares/verifyToken')
 const validateObjectId = require('../middlewares/validateObjectId')
 // api/users/profile
 router.get('/profil',verifyTokenAndAdmin,getAllUsers)
 // api/users/profile/:id
-router.get('/profil/:id',validateObjectId,getUser)
-
+router.route("/profile/:id")
+        .get(validateObjectId,getUser)
+        .put(validateObjectId,verifyTokenAndAdmin,updateUserProfilCtrl)
+// Count Users
+router.get('/count',verifyTokenAndAdmin,getUsersCountCtrl)
 module.exports = router
