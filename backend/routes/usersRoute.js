9bcf7b84b5bbec671ee131b895b3ea872aa0d819
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-16 17:48:20 +0100

    Configuration des routes

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,14 +1,15 @@
 const router = require('express').Router()
-const { getAllUsers, getUser, updateUserProfilCtrl, getUsersCountCtrl, profilePhotoUploadCtrl } = require('../controllers/usersControllers')
-const { verifyToken,verifyTokenAndAdmin ,verifyTokenAndOnlyUser} = require('../middlewares/verifyToken')
+const { getAllUsers, getUser, updateUserProfilCtrl, getUsersCountCtrl, profilePhotoUploadCtrl ,deleteUserProfilCtrl } = require('../controllers/usersControllers')
+const { verifyToken,verifyTokenAndAdmin ,verifyTokenAndOnlyUser, verifyTokenAndAuthorization} = require('../middlewares/verifyToken')
 const validateObjectId = require('../middlewares/validateObjectId')
 const photoUpload = require('../middlewares/photoUpload')
 // api/users/profile
-router.get('/profil',verifyTokenAndAdmin,getAllUsers)
+router.get('/profile',verifyTokenAndAdmin,getAllUsers)
 // api/users/profile/:id
 router.route("/profile/:id")
         .get(validateObjectId,getUser)
         .put(validateObjectId,verifyTokenAndAdmin,updateUserProfilCtrl)
+        .delete(validateObjectId,verifyTokenAndAuthorization,deleteUserProfilCtrl)
 // /api/users/profile/profile-photo-upload
 router.route('/profile/profile-photo-upload')
       .post(verifyToken ,photoUpload.single("image") ,profilePhotoUploadCtrl)
