14ba04c384a615faf6e0e1df000e7a5c84c2733f
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-16 16:52:28 +0100

    instalation et configuration de multer pour l enregistrement de photo et cloudinary pour la sauvgarder au service de cloudinary

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,13 +1,19 @@
 const router = require('express').Router()
-const { getAllUsers, getUser, updateUserProfilCtrl, getUsersCountCtrl } = require('../controllers/usersControllers')
-const { verifyTokenAndAdmin ,verifyTokenAndOnlyUser} = require('../middlewares/verifyToken')
+const { getAllUsers, getUser, updateUserProfilCtrl, getUsersCountCtrl, profilePhotoUploadCtrl } = require('../controllers/usersControllers')
+const { verifyToken,verifyTokenAndAdmin ,verifyTokenAndOnlyUser} = require('../middlewares/verifyToken')
 const validateObjectId = require('../middlewares/validateObjectId')
+const photoUpload = require('../middlewares/photoUpload')
 // api/users/profile
 router.get('/profil',verifyTokenAndAdmin,getAllUsers)
 // api/users/profile/:id
 router.route("/profile/:id")
         .get(validateObjectId,getUser)
         .put(validateObjectId,verifyTokenAndAdmin,updateUserProfilCtrl)
-// Count Users
+// /api/users/profile/profile-photo-upload
+router.route('/profile/profile-photo-upload')
+      .post(verifyToken ,photoUpload.single("image") ,profilePhotoUploadCtrl)
+        // Count Users
 router.get('/count',verifyTokenAndAdmin,getUsersCountCtrl)
+//
+
 module.exports = router
