49a4c558c056351b5fd5eba97ea87c8d512ea9e2
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-16 11:32:27 +0100

    ajouter le middleware qui permet de decider si l utulisateur a le drois pour quelques fonctions

---
+++ backend/middlewares/verifyToken.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken')
+
+
+// Verify Token 
+function verifyToken(req,res,next) {
+    const authToken = req.headers.authorization;
+    if(authToken) {
+        const token = authToken.split(' ')[1]
+        try {
+            const decodedPaylod = jwt.verify(token,process.env.PRIVATE_KEY);
+            req.user = decodedPaylod;
+            next();
+        } catch (error) {
+            return res.status(401).json({msg:"invalid token , acces denied"})
+        }
+    } else{
+        return res.status(401).json({msg:"no token provided , acces denied"})
+    }
+}
+
+
+// isAdmin
+function verifyTokenAndAdmin(req,res,next) {
+    verifyToken(req,res,()=>{
+        if(!req.user.isAdmin) {
+            res.status(401).json({msg:"not allowed , only admin"})
+        }else {
+            next()
+        }
+    })
+}
+
+module.exports = {
+    verifyToken,
+    verifyTokenAndAdmin
+}
