797ce2947a96178589afd17682bdc6a70e5f6b23
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-17 09:44:56 +0100

    ajouter le route pour FormationCtrl

--- backend/routes/formationRoute.js
+++ backend/routes/formationRoute.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
-const { createFormation, getAllFormationCtrl } = require('../controllers/FormationCtrl')
+const { createFormation, getAllFormationCtrl, getSingleFormationCtrl, getCountFormationCtrl, deleteFormationCtrl } = require('../controllers/FormationCtrl')
 const photoUpload = require('../middlewares/photoUpload')
 const {verifyToken, verifyTokenAndAdmin} = require('../middlewares/verifyToken')
-
+const validateObjectId = require('../middlewares/validateObjectId')
 //api/formation
 router.route('/')
       .post(verifyTokenAndAdmin,photoUpload.single("image"),createFormation)
       .get(getAllFormationCtrl)
+
+//api/formation/count
+router.route('/count').get(getCountFormationCtrl)
+//api/formation/:id
+router.route('/:id')
+      .get(validateObjectId,getSingleFormationCtrl)
+      .delete(validateObjectId,verifyToken,deleteFormationCtrl)   
 module.exports = router
