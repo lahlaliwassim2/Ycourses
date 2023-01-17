aab77ab7a7554cfb4663572260c9d8aada6d351e
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-17 14:07:02 +0100

    ajouter le route pour la fonction 'deleteCommentCtrl'

--- backend/routes/commentRoute.js
+++ backend/routes/commentRoute.js
@@ -1,9 +1,13 @@
-const { createCommentCtrl } = require('../controllers/commentCtrl');
-const { verifyToken } = require('../middlewares/verifyToken');
+const { createCommentCtrl, gettCommentCtrl, deleteCommentCtrl } = require('../controllers/commentCtrl');
+const validateObjectId = require('../middlewares/validateObjectId');
+const { verifyToken, verifyTokenAndAdmin } = require('../middlewares/verifyToken');
 
 const router = require('express').Router()
 
 // /api/comment
-router.route("/").post(verifyToken,createCommentCtrl)
-
+router.route("/")
+    .post(verifyToken,createCommentCtrl)
+    .get(verifyTokenAndAdmin,gettCommentCtrl)
+// /api/comment/:is
+router.route("/:id").delete(validateObjectId,verifyToken,deleteCommentCtrl)
 module.exports = router;
